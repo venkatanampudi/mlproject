066a5ba20df53103f5d510acd11273a719ab2914
Venkat Anampudi <venkat.anampudi.com>
2025-11-20 13:04:10 -0500

    exception handing with logging

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
-import sys # for python runtime environment
-import logging
+import sys
+from src.logger import logging
+
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
